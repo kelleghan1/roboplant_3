9eef6d0ae725abe7feaab4e94268572fff700670
Coleman Kelleghan <imdesigner2012@gmail.com>
2018-07-28 11:08:33 -0600

    updated server to user client side files for prod + dev differently

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const cors = require('cors');
+const dev = process.env.NODE_ENV === 'development';
 
 const io = require('socket.io')();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const port = 3000;
 const logger = require('morgan');
+const helpers = require('./helpers');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,11 +26,19 @@ app.set('socketio', io);
 
 app.use('/', index);
 
-app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/client'));
+
+// app.all('*', (req, res, next) => {
+//     res.sendFile('index.html', {
+//         root: __dirname + '/client'
+//     });
+// });
+
+app.use(express.static(dev ? helpers.root('client') : helpers.root('dist')));
 
 app.all('*', (req, res, next) => {
     res.sendFile('index.html', {
-        root: __dirname + '/client'
+        root: dev ? helpers.root('client') : helpers.root('dist')
     });
 });
 
